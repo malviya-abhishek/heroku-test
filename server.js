9786a1fd1d8a6958ed8f34172b199fd345c061bf
abhishek <abhismalviya@outlook.com>
2021-09-06 00:27:57 +0530

    index

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.send( `Hello ${ process.env.name }` )
+	res.sendFile( "/index.html" );
 });
 
 app.listen(PORT, () => {
