740eca2d583e14ef037f51d423b05f6a346b7ca4
abhishek <abhismalviya@outlook.com>
2021-09-06 00:21:24 +0530

    name change

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+require("dotenv").config()
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("ok")
+  res.send( `Hello ${ process.env.name }` )
 });
 
 app.listen(PORT, () => {
